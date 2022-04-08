b1839c4b5472434e9f518fc6810870a5508e33bf
Enzo Bomark <71819938+EnzoBomark@users.noreply.github.com>
2022-04-08 10:00:28 +0200

    docs: comment validate functions

--- packages/traits/validation/validator.ts
+++ packages/traits/validation/validator.ts
@@ -1,11 +1,13 @@
 import { v } from './validation';
 
+// Criteria
 type Validator = {
   validate: (str: string) => boolean;
   message: string;
 };
 
 export const validator = (str: string, ...criteria: Validator[]) =>
+  // Test string against array of criteria
   [v.notEmpty, ...criteria].reduceRight(
     (messages: string | undefined, criterion) =>
       criterion.validate(str) ? criterion.message : messages,
@@ -25,27 +27,38 @@ export const validate = <
 ) => {
   const keys = Array.from(map.keys());
   const entries = Array.from(map.entries());
-  const hash = keys.map((i) => Object.keys(i)).flat();
+
+  // Get entries of object and create iterable array of single object key value pairs
   const tests = Object.entries(obj).map((e) => ({ [e[0]]: e[1] }));
 
+  // Create single dimension array of object keys
+  const hash = keys.map((i) => Object.keys(i)).flat();
+
+  // Compare set of hash size to original hash for duplicate keys
   if (new Set(hash).size !== hash.length)
     throw new Error(`Map has duplicate keys, ${hash}`);
 
+  // Loop through each test
   const res = tests.map((test) => {
+    // Get criteria from Map
     const criteria = entries.find((i) => {
       return i.some((i) => Object.keys(i)[0] === Object.keys(test)[0]);
     });
 
+    // Get key value from Map
     const value = keys.find((i) => {
       return Object.keys(i)[0] === Object.keys(test)[0];
     });
 
+    // Return first invalid result
     return criteria
       ? validator(Object.values(value || {})[0] || '', ...criteria[1])
       : undefined;
   });
 
+  // Remove undefined values from array
   const messages = res.filter((i) => i);
 
+  // Return string for single obj and array of strings for multi obj
   return res.length <= 1 ? messages[0] : messages;
 };
