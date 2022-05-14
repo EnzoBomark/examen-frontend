5739a42b8d2aaa57792c10c9b6d978b835d3f012
Enzo Bomark <71819938+EnzoBomark@users.noreply.github.com>
2022-05-14 10:12:09 +0200

    feat: add text align

--- packages/styles/native/text/Label.tsx
+++ packages/styles/native/text/Label.tsx
@@ -5,4 +5,5 @@ export const Label = styled.Text<TextType>`
   font-size: ${theme.font.m};
   color: ${({ color }) => (color ? theme.colors[color] : theme.colors.g0)};
   font-weight: ${({ bold }) => (bold ? 700 : 400)};
+  text-align: ${({ align }) => align || 'left'};
 `;
